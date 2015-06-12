89998d92d8120eb213f589266c5cf0901ba2585f
Luke Zilioli <elzee@LZMacBookPro.local>
2015-06-11 21:57:31 -0500

    Add jsdoc flag to display warnings, clean up readme

--- grunt/config/jsdoc.js
+++ grunt/config/jsdoc.js
@@ -1,21 +1,25 @@
-var path = require( 'path' );
-module.exports = {
-	options: {
-		configure: '<%= sharedConfig.root %>/.jsdocrc',
-		template: path.join( __dirname, '../../node_modules/minami' )
-	},
-	dev: {
-		jsdoc: path.join( __dirname, '../../node_modules/.bin/jsdoc' ),
-		src: '<%= vars.paths.jsdoc %>',
+// https://www.npmjs.com/package/grunt-jsdoc
+
+module.exports = function(grunt) {
+	return {
 		options: {
-			destination: '<%= sharedConfig.curRepo %>/docs.ignore'
-		}
-	},
-	dist: {
-		jsdoc: path.join( __dirname, '../../node_modules/.bin/jsdoc' ),
-		src: '<%= vars.paths.jsdoc %>',
-		options: {
-			destination: '<%= sharedConfig.curRepo %>/docs'
+			configure: '<%= sharedConfig.root %>/.jsdocrc',
+			template: '<%= sharedConfig.root %>/node_modules/minami',
+			ignoreWarnings: !grunt.option('w-jsd')
+		},
+		dev: {
+			jsdoc: '<%= sharedConfig.root %>/node_modules/.bin/jsdoc',
+			src: '<%= vars.paths.jsdoc %>',
+			options: {
+				destination: '<%= sharedConfig.curRepo %>/docs.ignore'
+			}
+		},
+		dist: {
+			jsdoc: '<%= sharedConfig.root %>/node_modules/.bin/jsdoc',
+			src: '<%= vars.paths.jsdoc %>',
+			options: {
+				destination: '<%= sharedConfig.curRepo %>/docs'
+			}
 		}
 	}
 };
