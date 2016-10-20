4fe5acb70a8a46342bc86eb4dea4df8ceb52c57f
Luke Zilioli <elzee@LZMacBookPro.local>
2016-10-20 01:17:08 -0500

    switch to mainline grunt-release fixes #4

--- grunt/config/release.js
+++ grunt/config/release.js
@@ -1,33 +1,18 @@
 'use strict';
 
-var _ = require( 'lodash' );
-
 module.exports = function( grunt ) {
 	var pkg = grunt.file.readJSON( 'package.json' );
-
-	var noWrite = grunt.option( 'no-write' );
 	var gruntDebug = grunt.option( 'debug' );
 
 	grunt.registerTask(
 		'_logPublishDisabled',
-		'internal task',
+		'shared-grunt-config:internal task',
 		function() {
 			if ( pkg.private ) {
 				grunt.log.writeln( 'Package is private in package.json. Running grunt release will not publish it to npm.' );
 			}
 		}
 	);
-
-	var afterBumpArr = [];
-	if ( !noWrite ) {
-		afterBumpArr.push( 'jsdoc:dist' );
-	}
-
-	var filesToStageArr = _.filter( [
-		'docs/'
-	], function( f ) {
-		return grunt.file.exists( f );
-	} );
 
 	return {
 		options: {
@@ -36,20 +21,15 @@ module.exports = function( grunt ) {
 			changelog: true,
 			add: true,
 			noVerify: true,
-
 			commit: !gruntDebug,
 			tag: !gruntDebug,
 			push: !gruntDebug,
 			pushTags: !gruntDebug,
 			npm: !pkg.private && !gruntDebug,
-
 			commitMessage: '-- v<%= version %> RELEASE --',
 			tagName: 'v<%= version %>',
 			changelogText: '# v<%= version %> - **<%= grunt.template.today("yyyy-mm-dd") %>**\n',
-
-			afterBump: afterBumpArr,
-			beforeRelease: ['_logPublishDisabled', 'clean'],
-			filesToStage: filesToStageArr
+			beforeRelease: ['_logPublishDisabled']
 		}
 	};
 };
